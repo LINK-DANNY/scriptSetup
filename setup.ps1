61f64611b4776c59f5d067d29ba5c9319eba2309
DANLINK10 <ldfarinango@pucesi.edu.ec>
2025-06-15 18:34:31 -0500

    Primer commit

---
+++ setup.ps1
@@ -0,0 +1,75 @@
+# # Instalar IIS
+# Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature
+
+# # Descargar e instalar PHP (PHP 8.1 x64, por ejemplo)
+# Invoke-WebRequest -Uri "https://windows.php.net/downloads/releases/php-8.1.27-nts-Win32-vs16-x64.zip" -OutFile "C:\php.zip"
+# Expand-Archive "C:\php.zip" -DestinationPath "C:\php"
+
+# # Configurar PHP en el sistema
+# New-Item -Path "HKLM:\SOFTWARE\PHP" -Force
+# Set-ItemProperty -Path "HKLM:\SOFTWARE\PHP" -Name "InstallDir" -Value "C:\php"
+# [Environment]::SetEnvironmentVariable("PATH", $env:PATH + ";C:\php", [EnvironmentVariableTarget]::Machine)
+
+# # Configurar PHP en IIS
+# & $env:SystemRoot\System32\inetsrv\appcmd.exe set config /section:system.webServer/fastCgi /+[fullPath='C:\php\php-cgi.exe']
+# & $env:SystemRoot\System32\inetsrv\appcmd.exe set config /section:system.webServer/handlers /+[name='PHP-FastCGI',path='*.php',verb='GET,HEAD,POST',modules='FastCgiModule',scriptProcessor='C:\php\php-cgi.exe',resourceType='Either']
+
+# # Instalar Git
+# Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.45.1.windows.1/Git-2.45.1-64-bit.exe" -OutFile "C:\git_installer.exe"
+# Start-Process "C:\git_installer.exe" -ArgumentList "/VERYSILENT" -Wait
+
+# # Clonar el repositorio
+# git clone https://github.com/LINK-DANNY/panaderiaLaPasteleza.git C:\inetpub\wwwroot
+
+# # Permisos
+# icacls "C:\inetpub\wwwroot" /grant "IIS_IUSRS:(OI)(CI)(RX)" /T
+
+
+# URL del repo con token
+$repoUrl = "https://$gitToken@github.com/LINK-DANNY/insuPro.git"
+$sitePath = "C:\inetpub\insuPro"
+
+# Instalar Git
+$gitInstaller = "C:\git_installer.exe"
+Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.45.1.windows.1/Git-2.45.1-64-bit.exe" -OutFile $gitInstaller
+Start-Process -FilePath $gitInstaller -ArgumentList "/VERYSILENT" -Wait
+
+# Clonar proyecto
+git clone $repoUrl $sitePath
+
+# Instalar IIS
+Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+
+# Instalar URL Rewrite
+$rewriteUrl = "https://download.microsoft.com/download/D/D/9/DD9BD2E0-7ACD-4E29-A246-75C9DF9354F1/rewrite_amd64_en-US.msi"
+$rewriteInstaller = "$env:TEMP\rewrite.msi"
+Invoke-WebRequest -Uri $rewriteUrl -OutFile $rewriteInstaller
+Start-Process msiexec.exe -ArgumentList "/i $rewriteInstaller /quiet /norestart" -Wait
+
+# Descargar e instalar PHP
+$phpZip = "C:\php.zip"
+Invoke-WebRequest -Uri "https://windows.php.net/downloads/releases/php-8.1.27-nts-Win32-vs16-x64.zip" -OutFile $phpZip
+Expand-Archive -Path $phpZip -DestinationPath "C:\php"
+
+# Configurar PHP
+if (-not (Test-Path "HKLM:\SOFTWARE\PHP")) {
+    New-Item -Path "HKLM:\SOFTWARE\PHP" -Force
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\PHP" -Name "InstallDir" -Value "C:\php"
+[Environment]::SetEnvironmentVariable("PATH", $env:PATH + ";C:\php", [EnvironmentVariableTarget]::Machine)
+
+# Configurar FastCGI en IIS
+& "$env:SystemRoot\System32\inetsrv\appcmd.exe" set config /section:system.webServer/fastCgi /+"[fullPath='C:\php\php-cgi.exe']"
+& "$env:SystemRoot\System32\inetsrv\appcmd.exe" set config /section:system.webServer/handlers /+"[name='PHP-FastCGI',path='*.php',verb='GET,HEAD,POST',modules='FastCgiModule',scriptProcessor='C:\php\php-cgi.exe',resourceType='Either']"
+
+# Crear sitio en IIS
+Import-Module WebAdministration
+if (Test-Path "IIS:\Sites\insuPro") {
+    Remove-Website -Name "insuPro"
+}
+New-Website -Name "insuPro" -Port 8081 -PhysicalPath $sitePath -ApplicationPool ".NET v4.5" -Force
+
+# Permisos
+icacls $sitePath /grant "IIS_IUSRS:(OI)(CI)(RX)" /T
+
+Write-Host "✔️ Proyecto insuPro desplegado en http://localhost:8081"
