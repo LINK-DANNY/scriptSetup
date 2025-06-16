d609002efb00caad2ceac02c685610d471c855ce
DANLINK10 <ldfarinango@pucesi.edu.ec>
2025-06-15 21:34:43 -0500

    fix: path agregado de git 2

--- setup.ps1
+++ setup.ps1
@@ -2,58 +2,68 @@ param (
     [string]$gitToken
 )
 
-# URL del repo con token
+# URL del repo con token personal
 $repoUrl = "https://$gitToken@github.com/LINK-DANNY/insuPro.git"
 $sitePath = "C:\inetpub\insuPro"
 
-# Instalar Git
+# 1. Instalar Git
 $gitInstaller = "C:\git_installer.exe"
 Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.45.1.windows.1/Git-2.45.1-64-bit.exe" -OutFile $gitInstaller
 Start-Process -FilePath $gitInstaller -ArgumentList "/VERYSILENT" -Wait
 
-# Agregar Git al PATH (si se instaló correctamente)
+# 2. Agregar Git al PATH del sistema
 $gitCmdPath = "C:\Program Files\Git\cmd"
 if (Test-Path $gitCmdPath) {
     [Environment]::SetEnvironmentVariable("PATH", $env:PATH + ";$gitCmdPath", [EnvironmentVariableTarget]::Machine)
+    # También actualizar para la sesión actual
+    $env:PATH += ";$gitCmdPath"
 }
 
-# Clonar proyecto desde GitHub
+# 3. Confirmar que Git está disponible
+if (Get-Command git -ErrorAction SilentlyContinue) {
+    Write-Host "Git instalado correctamente."
+} else {
+    Write-Host "Git no está disponible. Revisa la instalación."
+}
+
+# 4. Clonar el proyecto desde GitHub
 git clone $repoUrl $sitePath
 
-# Instalar IIS
+# 5. Instalar IIS
 Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
 
-# Instalar URL Rewrite
+# 6. Instalar URL Rewrite
 $rewriteUrl = "https://download.microsoft.com/download/D/D/9/DD9BD2E0-7ACD-4E29-A246-75C9DF9354F1/rewrite_amd64_en-US.msi"
 $rewriteInstaller = "$env:TEMP\rewrite.msi"
 Invoke-WebRequest -Uri $rewriteUrl -OutFile $rewriteInstaller
 Start-Process msiexec.exe -ArgumentList "/i $rewriteInstaller /quiet /norestart" -Wait
 
-# Descargar e instalar PHP
+# 7. Descargar e instalar PHP
 $phpZip = "C:\php.zip"
 Invoke-WebRequest -Uri "https://windows.php.net/downloads/releases/php-8.1.27-nts-Win32-vs16-x64.zip" -OutFile $phpZip
 Expand-Archive -Path $phpZip -DestinationPath "C:\php"
 
-# Configurar PHP
+# 8. Configurar PHP
 if (-not (Test-Path "HKLM:\SOFTWARE\PHP")) {
     New-Item -Path "HKLM:\SOFTWARE\PHP" -Force
 }
 Set-ItemProperty -Path "HKLM:\SOFTWARE\PHP" -Name "InstallDir" -Value "C:\php"
 [Environment]::SetEnvironmentVariable("PATH", $env:PATH + ";C:\php", [EnvironmentVariableTarget]::Machine)
+$env:PATH += ";C:\php"
 
-# Configurar FastCGI en IIS
+# 9. Configurar FastCGI en IIS
 & "$env:SystemRoot\System32\inetsrv\appcmd.exe" set config /section:system.webServer/fastCgi /+"[fullPath='C:\php\php-cgi.exe']"
 & "$env:SystemRoot\System32\inetsrv\appcmd.exe" set config /section:system.webServer/handlers /+"[name='PHP-FastCGI',path='*.php',verb='GET,HEAD,POST',modules='FastCgiModule',scriptProcessor='C:\php\php-cgi.exe',resourceType='Either']"
 
-# Crear sitio en IIS
+# 10. Crear sitio en IIS
 Import-Module WebAdministration
 if (Test-Path "IIS:\Sites\insuPro") {
     Remove-Website -Name "insuPro"
 }
 New-Website -Name "insuPro" -Port 8081 -PhysicalPath $sitePath -ApplicationPool ".NET v4.5" -Force
 
-# Asignar permisos
+# 11. Asignar permisos
 icacls $sitePath /grant "IIS_IUSRS:(OI)(CI)(RX)" /T
 
-# Mensaje final
+# 12. Mensaje final
 Write-Host "Proyecto insuPro desplegado en http://localhost:8081"
